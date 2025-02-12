ac671fb9a14e39f49b4b7967391f643a204a67e5
tomerle03 <tomerle03@gmail.com>
2025-02-12 15:06:55 +0200

    basic operation is good

--- api/v1/ghost_types.go
+++ api/v1/ghost_types.go
@@ -25,17 +25,14 @@ import (
 
 // GhostSpec defines the desired state of Ghost.
 type GhostSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Ghost. Edit ghost_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	//+kubebuilder:validation:Pattern=`^[-a-z0-9]*$`
+	//The image tag of the Ghost container to deploy
+	ImageTag string `json:"imageTag"`
 }
 
 // GhostStatus defines the observed state of Ghost.
 type GhostStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
